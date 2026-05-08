f49192161da05912b7d6d2a4de48b1fc22090e80
ccamilomotta-crypto <ccamilomotta@gmail.com>
2026-05-07 22:07:47 -0500

    Introduce rank_functions.h for driver data handling

---
+++ rank_functions.h
@@ -0,0 +1,22 @@
+// this file is provided.  do not modify. do not submit.
+#ifndef PROVIDED_RANK_FUNCTIONS_H_
+#define PROVIDED_RANK_FUNCTIONS_H_
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+struct driver {
+    std::string lastname;
+    std::string country;
+    unsigned int number;
+    double time;
+    unsigned int rank;
+};
+
+std::string trim(std::string);
+std::vector<driver> load_driver_data(std::ifstream&);
+std::vector<driver> set_rankings(std::vector<driver>);
+void print_results(const std::vector<driver>&);
+
+#endif  // PROVIDED_RANK_FUNCTIONS_H_
